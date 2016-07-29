98170f45ffb64a9640f3948ab297ff6d867900f0
Tariq Abusheikh <tariq.abusheikh@gmail.com>
2016-07-29 13:50:27 -0500

    Add CSV return test

--- tests/CountryListTest.php
+++ tests/CountryListTest.php
@@ -20,13 +20,13 @@ class CountryListTest extends TestCase
     }
 
     /** @test */
-    public function constructor_set_data_directory() {
+    public function constructor_sets_data_directory() {
         $this->assertAttributeNotEmpty('dataDir', $this->countryList);
     }
 
     /** @test */
     public function it_can_get_one_country() {
-        $this->assertEquals("United States", $this->countryList->getOne('US', 'en'));
+        $this->assertEquals($this->_testCountryName, $this->countryList->getOne($this->_testCountryCode, 'en'));
     }
 
     /** @test */
@@ -36,12 +36,32 @@ class CountryListTest extends TestCase
     }
 
     /** @test */
+    public function it_can_return_php() {
+        $this->assertArrayHasKey($this->_testCountryCode, $this->countryList->getList('en', 'php'));
+    }
+
+    /** @test */
+    public function it_can_return_csv() {
+        $countries = str_getcsv($this->countryList->getList('en', 'csv'), "\n");
+        if (is_array($countries)) {
+            array_shift($countries);
+            $c = [];
+            foreach ($countries as $country) {
+                $x = explode(',', $country);
+                $c[$x[0]] = preg_replace('/\"/', '', $x[1]);
+            }
+            $countries = $c;
+        }
+        $this->assertArrayHasKey($this->_testCountryCode, $countries);
+    }
+
+    /** @test */
     public function it_can_check_for_a_country() {
         $this->assertTrue($this->countryList->has($this->_testCountryCode));
     }
 
     /** @test */
-    public function it_throws_an_exception_for_invalid_countries() {
+    public function it_throws_an_exception_for_invalid_country() {
         $this->expectException(CountryNotFoundException::class);
         $this->countryList->getOne('asdne');
     }
